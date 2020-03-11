38ffba826a0c9af57e6c17910ada0c287885e885
Juan <lookinmeal@gmail.com>
2020-03-11 18:55:26 +0100

    distance in tiles

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:lookinmeal/models/restaurant.dart';
 import 'package:lookinmeal/services/database.dart';
+import 'package:lookinmeal/services/geolocation.dart';
 import 'package:lookinmeal/services/json_update.dart';
 import 'package:lookinmeal/shared/loading.dart';
 
@@ -12,7 +14,10 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
 	DBService _dbService = DBService();
+	GeolocationService _geolocationService = GeolocationService();
+	Position myPos;
 	List<Restaurant> restaurants;
+	List<double> distances = List<double>();
 
 	@override
 	initState(){
@@ -21,41 +26,45 @@ class _HomeScreenState extends State<HomeScreen> {
 	}
 	void _update()async{
 		restaurants = await _dbService.allrestaurantdata;
+		myPos = await _geolocationService.getLocation();
+		for(Restaurant restaurant in restaurants){
+			distances.add(await _geolocationService.distanceBetween(myPos.latitude,myPos.longitude, restaurant.latitude, restaurant.longitude));
+		}
 	}
 	@override
   Widget build(BuildContext context) {
-    return restaurants == null ? Loading() : Container(
+    return restaurants == null || distances.length < 4 ? Loading() : Container(
 		child: ListView(
 			children: <Widget>[
 				Card(
 				  child: ListTile(
 				  		title: Text(restaurants.elementAt(0).name),
-				  		subtitle: Text(restaurants.elementAt(0).address),
-				  		leading: Image.network(restaurants.elementAt(0).images.first),
+				  		subtitle: Text(distances.elementAt(0).toString()),
+				  		leading: Image.network(restaurants.elementAt(0).images.first, width: 100, height: 100,),
 				  		trailing: Icon(Icons.arrow_right),
 				  ),
 				),
 				Card(
 					child: ListTile(
 						title: Text(restaurants.elementAt(1).name),
-						subtitle: Text(restaurants.elementAt(1).address),
-						leading: Image.network(restaurants.elementAt(1).images.first),
+						subtitle: Text(distances.elementAt(1).toString()),
+						leading: Image.network(restaurants.elementAt(1).images.first, width: 100, height: 100,),
 						trailing: Icon(Icons.arrow_right),
 					),
 				),
 				Card(
 					child: ListTile(
 						title: Text(restaurants.elementAt(2).name),
-						subtitle: Text(restaurants.elementAt(2).address),
-						leading: Image.network(restaurants.elementAt(2).images.first),
+						subtitle: Text(distances.elementAt(2).toString()),
+						leading: Image.network(restaurants.elementAt(2).images.first, width: 100, height: 100,),
 						trailing: Icon(Icons.arrow_right),
 					),
 				),
 				Card(
 					child: ListTile(
 						title: Text(restaurants.elementAt(3).name),
-						subtitle: Text(restaurants.elementAt(3).address),
-						leading: Image.network(restaurants.elementAt(3).images.first),
+						subtitle: Text(distances.elementAt(3).toString()),
+						leading: Image.network(restaurants.elementAt(3).images.first, width: 100, height: 100,),
 						trailing: Icon(Icons.arrow_right),
 					),
 				)
--- lib/services/geolocation.dart
+++ lib/services/geolocation.dart
@@ -2,6 +2,11 @@ import 'package:geolocator/geolocator.dart';
 
 class GeolocationService{
 
+	Future<double> distanceBetween(double startLatitude, double startLongitude, double endLatitude, double endLongitude)async{ //optimizable
+		double distance = await Geolocator().distanceBetween(startLatitude, startLongitude, endLatitude, endLongitude);
+		return num.parse((distance/1000).toStringAsFixed(2));
+	}
+
 	Future<Position> getLocation() async{
 		Position position = await Geolocator().getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
 		if(position == null)
