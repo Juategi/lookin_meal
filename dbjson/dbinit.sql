c1c34a94dcc2be86d965a57eb12980c3e63f8e83
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-03-29 00:04:20 +0100

    minor

--- dbjson/dbinit.sql
+++ dbjson/dbinit.sql
@@ -1,4 +1,6 @@
-create table restaurant(restaurant_id serial PRIMARY KEY, ta_id integer unique, name VARCHAR (150) NOT NULL, address VARCHAR (150) NOT NULL, city VARCHAR (50) NOT NULL, country VARCHAR (50) NOT NULL, email VARCHAR (50), phone VARCHAR (50), website VARCHAR (50), types text[], images text[], schedule json, rating real not null, latitude real not null, longitude real not null, numRevta integer, sections text[], currency varchar(5)); 
+begin;
+
+create table restaurant(restaurant_id serial PRIMARY KEY, ta_id integer unique, name VARCHAR (150) NOT NULL, address VARCHAR (150) NOT NULL, city VARCHAR (50) NOT NULL, country VARCHAR (50) NOT NULL, email VARCHAR (50), phone VARCHAR (50), website VARCHAR (50), types text[], images text[], schedule json, rating real not null, latitude real not null, longitude real not null, numRevta integer, sections text[], currency varchar(5));
 
 create table menuentry (entry_id serial PRIMARY KEY, restaurant_id integer not null, name VARCHAR (150) NOT NULL, section VARCHAR (150) NOT NULL, rating real not null, numReviews integer not null, price real, CONSTRAINT restaurant_to_entry_fkey FOREIGN KEY (restaurant_id) REFERENCES restaurant (restaurant_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE ); 
 
@@ -10,3 +12,4 @@ create table owner(user_id varchar(50)  NOT NULL, restaurant_id integer NOT NULL
 
 create table rating(user_id varchar(50)  NOT NULL, entry_id integer NOT NULL, rating real not null, PRIMARY KEY (user_id, entry_id), CONSTRAINT rating_menuentry_fkey FOREIGN KEY (entry_id) REFERENCES menuentry (entry_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE, CONSTRAINT rating_users_fkey FOREIGN KEY (user_id) REFERENCES users (user_id) MATCH SIMPLE ON UPDATE CASCADE ON DELETE CASCADE); 
 
+commit;
