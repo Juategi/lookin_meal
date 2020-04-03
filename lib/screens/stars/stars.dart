ce8e917e300c064c59a283534e2b0bf8f3bbc170
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-04-03 12:48:56 +0200

    creating new functions

--- lib/screens/stars/stars.dart
+++ lib/screens/stars/stars.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:lookinmeal/models/user.dart';
+import 'package:lookinmeal/services/database.dart';
 import 'package:lookinmeal/services/json_update.dart';
 import 'package:lookinmeal/services/newdatabase.dart';
 import 'package:lookinmeal/services/search.dart';
+import 'package:provider/provider.dart';
 
 class Stars extends StatefulWidget {
   @override
@@ -9,13 +12,24 @@ class Stars extends StatefulWidget {
 }
 
 class _StarsState extends State<Stars> {
+  User user;
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: RaisedButton(
-        //onPressed: ()async{JsonUpdate().updateFromJson("valencia_tripad.json",331);},
-        onPressed: ()async{DBServiceN dbServiceN = DBServiceN(); dbServiceN.getAllRestaurants();},
-      ),
+    user = Provider.of<User>(context);
+    return StreamBuilder<User>(
+      stream: DBService(uid: user.uid).userdata,
+      builder: (context, snapshot) {
+        user = snapshot.data;
+        return Container(
+          child: RaisedButton(
+            child: Text("Prueba"),
+            //onPressed: ()async{JsonUpdate().updateFromJson("valencia_tripad.json",331);},
+            //onPressed: ()async{DBServiceN dbServiceN = DBServiceN(); dbServiceN.getAllRestaurants();},
+            //onPressed: () async {DBServiceN dbServiceN = DBServiceN();dbServiceN.createUser(user.uid, user.email, user.name, user.picture, user.service);},
+            onPressed: ()async{DBServiceN dbServiceN = DBServiceN(); dbServiceN.getUserData(user.uid);},
+          ),
+        );
+      }
     );
   }
 }
--- lib/services/newdatabase.dart
+++ lib/services/newdatabase.dart
@@ -1,27 +1,101 @@
 import 'dart:convert';
-import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
-import 'package:html/parser.dart' show parse;
 import 'package:lookinmeal/models/restaurant.dart';
 import 'package:lookinmeal/models/user.dart';
 
 class DBServiceN{
-
-  //URL: https://lookinmeal-dcf41.firebaseapp.com
-  //https://us-central1-lookinmeal-dcf41.cloudfunctions.net/app
-  String uid;
-
-  Future updateUserData(String email, String name, String picture, String service ) async {
-   
+  
+  Future createUser(String id, String email, String name, String picture, String service) async {
+    Map body = {
+      "id" :id,
+      "name":name,
+      "email":email,
+      "service":service,
+      "image":picture
+    };
+    var response = await http.post("https://lookinmeal-dcf41.firebaseapp.com/users", body: body);
+    print(response.body);
   }
 
-  Future updateUserImage(String picture,String uid) async {
-
+  Future updateUserData(String id, String email, String name, String picture, String service) async {
+    Map body = {
+      "id" :id,
+      "name":name,
+      "email":email,
+      "service":service,
+      "image":picture
+    };
+    var response = await http.put("https://lookinmeal-dcf41.firebaseapp.com/users", body: body);
+    print(response.body);
   }
 
 
-  Future updateUserFavorites(String id) async{
+  Future<List<Restaurant>> updateUserFavorites(String userId, String restaurantId) async{
 
+  }
+
+  Future<List<Restaurant>> getUserFavorites(String id) async{
+    var response = await http.get("https://lookinmeal-dcf41.firebaseapp.com/userfavs", headers: {"id":id});
+    List<Restaurant> restaurants = List<Restaurant>();
+    List<dynamic> result = json.decode(response.body);
+    Map<String,List<int>> schedule;
+    for(dynamic element in result){
+      schedule = {'1': new List<int>(), '2': new List<int>(), '3': new List<int>(), '4': new List<int>(), '5': new List<int>(), '6': new List<int>(), '0': new List<int>()};
+      if(element['schedule'] != null) {
+        dynamic result = json.decode(element['schedule'].toString()
+            .replaceAll("0:", '"0":')
+            .replaceAll("1:", '"1":')
+            .replaceAll("2:", '"2":')
+            .replaceAll("3:", '"3":')
+            .replaceAll("4:", '"4":')
+            .replaceAll("5:", '"5":')
+            .replaceAll("6:", '"6":')
+        );
+        for (int i = 0; i < 7; i++) {
+          for (dynamic hour in result[i.toString()].toList()) {
+            schedule[i.toString()].add(hour);
+          }
+        }
+      }
+      Restaurant restaurant = Restaurant(
+          restaurant_id : element['restaurant_id'].toString(),
+          ta_id: element['ta_id'].toString(),
+          name: element['name'],
+          phone: element['phone'],
+          website: element['website'],
+          webUrl: element['weburl'],
+          address: element['address'],
+          city: element['city'],
+          country: element['country'],
+          latitude: element['latitude'],
+          longitude: element['longitude'],
+          rating: double.parse(element['rating'].toString()),
+          numrevta: element['numrevta'],
+          images: element['images'] == null ? null : List<String>.from(element['images']),
+          types: element['types'] == null ? null : List<String>.from(element['types']),
+          schedule: schedule,
+          currency: element['currency'],
+          sections: element['sections'] == null ? null : List<String>.from(element['sections'])
+      );
+      restaurants.add(restaurant);
+    }
+    print("Number of restaurants : ${restaurants.length}");
+    return restaurants;
+  }
+
+
+  Stream<User> getUserData(String id) async*{
+    var response = await http.get("https://lookinmeal-dcf41.firebaseapp.com/users", headers: {"id":id});
+    List<dynamic> result = json.decode(response.body);
+    User user = User(
+      uid: result.first["user_id"],
+      name: result.first["name"],
+      email: result.first["email"],
+      service: result.first["service"],
+      picture: result.first["image"],
+    );
+    print("User obtained: ${result.first}");
+    yield user;
   }
 
   Future updateRestaurantData(String taId, String name, String phone, String website, String webUrl, String address, String email, String city, String country, double latitude,
@@ -48,8 +122,6 @@ class DBServiceN{
     var response = await http.post("https://lookinmeal-dcf41.firebaseapp.com/restaurants", body: body);
     print(response.body);
   }
-
-
 
   Future<List<Restaurant>> getAllRestaurants() async{
     var response = await http.get("https://lookinmeal-dcf41.firebaseapp.com/allrestaurants");
@@ -96,16 +168,8 @@ class DBServiceN{
       );
       restaurants.add(restaurant);
     }
+    print("Number of restaurants : ${restaurants.length}");
     return restaurants;
-  }
-
-  Future<List<Restaurant>> getFavorites(List<String> ids) async{
-
-  }
-
-
-  Stream<User> get userdata{
-
   }
 
 }
