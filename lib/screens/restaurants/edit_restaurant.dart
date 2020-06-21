274fa9046b9526bab3ebab748f04644e53ac1dc4
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2020-06-21 13:50:47 +0200

    schdule edit

--- lib/screens/restaurants/edit_restaurant.dart
+++ lib/screens/restaurants/edit_restaurant.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:lookinmeal/models/menu_entry.dart';
@@ -21,7 +22,12 @@ class _EditRestaurantState extends State<EditRestaurant> {
   List<String> images, types;
   Map<String, List<int>> schedule;
   Restaurant restaurant;
-
+  @override
+  void initState() {
+    schedule = {'1': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '2': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '3': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '4': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '5': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '6': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1), '0': new List<int>()..add(-1)..add(-1)..add(-1)..add(-1)};
+    print(schedule);
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
     restaurant = ModalRoute.of(context).settings.arguments;
@@ -40,7 +46,7 @@ class _EditRestaurantState extends State<EditRestaurant> {
               SizedBox(height: 10,),
               TextFormField(
                 onChanged: (value){
-                  setState(() => name = value);
+                  name = value;
                 },
                 controller: TextEditingController()..text = restaurant.name,
                 validator: (val) => val.length < 4 || val.length > 120 ? "Mínimo 4 carácteres y menos de 120" : null,
@@ -54,7 +60,7 @@ class _EditRestaurantState extends State<EditRestaurant> {
               SizedBox(height: 10,),
               TextFormField(
                 onChanged: (value){
-                  setState(() => email = value);
+                  email = value;
                 },
                 controller: TextEditingController()..text = restaurant.email,
                 validator: (val) => !RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+").hasMatch(val) ? "Introduce un email válido" : null,
@@ -68,7 +74,7 @@ class _EditRestaurantState extends State<EditRestaurant> {
               SizedBox(height: 10,),
               TextFormField(
                 onChanged: (value){
-                  setState(() => web = value);
+                  web = value;
                 },
                 controller: TextEditingController()..text = restaurant.website,
                 validator: (val) => val.length < 4 || val.length > 149 ? "Mínimo 4 carácteres y menos de 150" : null,
@@ -82,7 +88,7 @@ class _EditRestaurantState extends State<EditRestaurant> {
               SizedBox(height: 10,),
               TextFormField(
                 onChanged: (value){
-                  setState(() => phone = value);
+                  phone = value;
                 },
                 controller: TextEditingController()..text = restaurant.phone,
                 validator: (val) => val.length < 6 || val.length > 49 ? "Mínimo 6 carácteres y menos de 50" : null,
@@ -96,7 +102,7 @@ class _EditRestaurantState extends State<EditRestaurant> {
               SizedBox(height: 10,),
               TextFormField(
                 onChanged: (value){
-                  setState(() => address = value);
+                  address = value;
                 },
                 controller: TextEditingController()..text = restaurant.address,
                 validator: (val) => val.length < 4 || val.length > 149 ? "Mínimo 4 carácteres y menos de 150" : null,
@@ -115,32 +121,174 @@ class _EditRestaurantState extends State<EditRestaurant> {
                   },
                   errorText: 'Please select one or more option(s)',
                   dataSource: [
-                    {
-                      "display": "Australia",
-                      "value": 1,
-                    },
-                    {
-                      "display": "Canada",
-                      "value": 2,
-                    },
-                    {
-                      "display": "India",
-                      "value": 3,
-                    },
-                    {
-                      "display": "United States",
-                      "value": 4,
-                    }
+                    {"value": "Café"},
+                    {"value": "Afghan"},
+                    {"value": "Afghani"},
+                    {"value": "African"},
+                    {"value": "American"},
+                    {"value": "Argentinean"},
+                    {"value": "Armenian"},
+                    {"value": "Asian"},
+                    {"value": "Asian Fusion"},
+                    {"value": "Australian"},
+                    {"value": "Austrian"},
+                    {"value": "Bagels"},
+                    {"value": "Bahamian"},
+                    {"value": "Bakery"},
+                    {"value": "Balti"},
+                    {"value": "Bangladeshi"},
+                    {"value": "Bar"},
+                    {"value": "Barbeque"},
+                    {"value": "Basque"},
+                    {"value": "Belgian"},
+                    {"value": "Bistro"},
+                    {"value": "Brasserie"},
+                    {"value": "Brazilian"},
+                    {"value": "Brew Pub"},
+                    {"value": "British"},
+                    {"value": "Burmese"},
+                    {"value": "Cajun & Creole"},
+                    {"value": "Californian"},
+                    {"value": "Cambodian"},
+                    {"value": "Canadian"},
+                    {"value": "Caribbean"},
+                    {"value": "Central American"},
+                    {"value": "Central European"},
+                    {"value": "Chicken Wings"},
+                    {"value": "Chilean"},
+                    {"value": "Chinese"},
+                    {"value": "Chowder"},
+                    {"value": "Coffee Shop"},
+                    {"value": "Colombian"},
+                    {"value": "Contemporary"},
+                    {"value": "Continental"},
+                    {"value": "Costa Rican"},
+                    {"value": "Creperie"},
+                    {"value": "Croatian"},
+                    {"value": "Cuban"},
+                    {"value": "Czech"},
+                    {"value": "Danish"},
+                    {"value": "Delicatessen"},
+                    {"value": "Dessert"},
+                    {"value": "Dim Sum"},
+                    {"value": "Diner"},
+                    {"value": "Donuts"},
+                    {"value": "Dutch"},
+                    {"value": "Eastern European"},
+                    {"value": "Eclectic"},
+                    {"value": "Ecuadorean"},
+                    {"value": "Egyptian"},
+                    {"value": "English"},
+                    {"value": "Ethiopian"},
+                    {"value": "European"},
+                    {"value": "Family Fare"},
+                    {"value": "Fast Food"},
+                    {"value": "Filipino"},
+                    {"value": "Fish & Chips"},
+                    {"value": "Fondue"},
+                    {"value": "French"},
+                    {"value": "Fusion"},
+                    {"value": "Gastropub"},
+                    {"value": "German"},
+                    {"value": "Greek"},
+                    {"value": "Grill"},
+                    {"value": "Guatemalan"},
+                    {"value": "Gluten Free Options"},
+                    {"value": "Halal"},
+                    {"value": "Hamburgers"},
+                    {"value": "Hawaiian"},
+                    {"value": "Healthy"},
+                    {"value": "Hot Dogs"},
+                    {"value": "Hunan"},
+                    {"value": "Hungarian"},
+                    {"value": "Ice Cream"},
+                    {"value": "Indian"},
+                    {"value": "Indonesian"},
+                    {"value": "International"},
+                    {"value": "Irish"},
+                    {"value": "Israeli"},
+                    {"value": "Italian"},
+                    {"value": "Jamaican"},
+                    {"value": "Japanese"},
+                    {"value": "Korean"},
+                    {"value": "Kosher"},
+                    {"value": "Latin"},
+                    {"value": "Latvian"},
+                    {"value": "Lebanese"},
+                    {"value": "Malaysian"},
+                    {"value": "Mediterranean"},
+                    {"value": "Mexican"},
+                    {"value": "Middle Eastern"},
+                    {"value": "Mongolian"},
+                    {"value": "Moroccan"},
+                    {"value": "Native American"},
+                    {"value": "Nepali"},
+                    {"value": "New Zealand"},
+                    {"value": "Nonya"},
+                    {"value": "Noodle"},
+                    {"value": "Noodle Shop"},
+                    {"value": "Norwegian"},
+                    {"value": "Organic"},
+                    {"value": "Oyster Bar"},
+                    {"value": "Pacific Rim"},
+                    {"value": "Pakistani"},
+                    {"value": "Pan-Asian"},
+                    {"value": "Pasta"},
+                    {"value": "Peruvian"},
+                    {"value": "Philippine"},
+                    {"value": "Pizza"},
+                    {"value": "Pizza & Pasta"},
+                    {"value": "Polish"},
+                    {"value": "Polynesian"},
+                    {"value": "Portuguese"},
+                    {"value": "Pub"},
+                    {"value": "Puerto Rican"},
+                    {"value": "Romanian"},
+                    {"value": "Russian"},
+                    {"value": "Salvadoran"},
+                    {"value": "Sandwiches"},
+                    {"value": "Scandinavian"},
+                    {"value": "Scottish"},
+                    {"value": "Seafood"},
+                    {"value": "Singaporean"},
+                    {"value": "Slovenian"},
+                    {"value": "Soups"},
+                    {"value": "South American"},
+                    {"value": "Southwestern"},
+                    {"value": "Spanish"},
+                    {"value": "Sri Lankan"},
+                    {"value": "Steakhouse"},
+                    {"value": "Street Food"},
+                    {"value": "Sushi"},
+                    {"value": "Swedish"},
+                    {"value": "Swiss"},
+                    {"value": "Szechuan"},
+                    {"value": "Taiwanese"},
+                    {"value": "Tapas"},
+                    {"value": "Tea Room"},
+                    {"value": "Thai"},
+                    {"value": "Tibetan"},
+                    {"value": "Tunisian"},
+                    {"value": "Turkish"},
+                    {"value": "Ukrainian"},
+                    {"value": "Vegan Options"},
+                    {"value": "Vegetarian Friendly"},
+                    {"value": "Venezuelan"},
+                    {"value": "Vietnamese"},
+                    {"value": "Welsh"},
+                    {"value": "Wine Bar"},
+                    {"value": "Winery"},
+                    {"value": "Yugoslavian"},
                   ],
-                  textField: 'display',
+                  textField: 'value',
                   valueField: 'value',
                   filterable: true,
                   required: true,
                   value: null,
-                  initialValue: [1,3],
+                  initialValue: restaurant.types,
                   maxLength: 20,
                   change: (value) {
-                    print('The value is $value');
+                    types = List<String>.from(value);
                   },
               ),),
               SizedBox(height: 20,),
@@ -148,10 +296,78 @@ class _EditRestaurantState extends State<EditRestaurant> {
                 padding: const EdgeInsets.only(right: 280),
                 child: Text("Horario", style: TextStyle(fontWeight: FontWeight.bold,color: Colors.black, fontSize: 13),),
               ),
+              SizedBox(height: 20,),
+              Text("Lunes:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(1),),
+              SizedBox(height: 25,),
+              Text("Martes:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(2),),
+              SizedBox(height: 25,),
+              Text("Miercoles:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(3),),
+              SizedBox(height: 25,),
+              Text("Jueves:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(4),),
+              SizedBox(height: 25,),
+              Text("Viernes:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(5),),
+              SizedBox(height: 25,),
+              Text("Sábado:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(6),),
+              SizedBox(height: 25,),
+              Text("Domingo:"),
+              SizedBox(height: 5,),
+              Row(children: _initRow(0),),
+              SizedBox(height: 25,),
             ],),
           ),)
       ],)
     );
+  }
+
+  List<Widget> _initRow(int i){
+    List<Widget> items = List<Widget>();
+    if(restaurant.schedule[i.toString()].length == 2){
+      restaurant.schedule[i.toString()].add(-1);
+      restaurant.schedule[i.toString()].add(-1);
+    }
+    else if(restaurant.schedule[i.toString()].length == 0){
+      restaurant.schedule[i.toString()].add(-1);
+      restaurant.schedule[i.toString()].add(-1);
+      restaurant.schedule[i.toString()].add(-1);
+      restaurant.schedule[i.toString()].add(-1);
+    }
+
+    for(int j = 0; j < 4; j++){
+      schedule[i.toString()][j] = restaurant.schedule[i.toString()][j];
+      items.add(
+        Container( width: 66,
+          child: TextFormField(
+            onChanged: (value){
+              if(value == "")
+                schedule[i.toString()][j] = -1;
+              else
+                schedule[i.toString()][j] = int.parse(value);
+              print(schedule);
+            },
+            initialValue: restaurant.schedule[i.toString()][j] == -1? null : restaurant.schedule[i.toString()][j].toString(),
+            decoration: textInputDeco.copyWith(hintText: "00"),
+            keyboardType: TextInputType.number,
+          ),
+        ),
+      );
+      if(j == 0 || j == 2)
+        items.add(Text("   -   "));
+      else if(j == 1)
+        items.add(SizedBox( width: 20,));
+    }
+    return items;
   }
 }
 
