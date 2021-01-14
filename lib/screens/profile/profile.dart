cb54278bcae44a44e00f193829badbbb8ec417e8
LookinMeal <61567119+LookinMeal@users.noreply.github.com>
2021-01-14 21:26:03 +0100

    profile

--- lib/screens/profile/profile.dart
+++ lib/screens/profile/profile.dart
@@ -1,13 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:lookinmeal/models/menu_entry.dart';
 import 'package:lookinmeal/models/restaurant.dart';
 import 'package:lookinmeal/models/user.dart';
 import 'package:lookinmeal/services/app_localizations.dart';
 import 'package:lookinmeal/services/auth.dart';
 import 'package:lookinmeal/services/database.dart';
-import 'package:lookinmeal/services/geolocation.dart';
-import 'package:lookinmeal/services/json_update.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:provider/provider.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:lookinmeal/shared/common_data.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -19,46 +22,188 @@ class _ProfileState extends State<Profile> {
   User user;
   @override
   Widget build(BuildContext context) {
+    ScreenUtil.init(context, height: CommonData.screenHeight, width: CommonData.screenWidth, allowFontScaling: true);
     user = Provider.of<User>(context);
     AppLocalizations tr = AppLocalizations.of(context);
-    return Column(
-      children: <Widget>[
-        Container(
-          child: Image.network(user.picture, height: 150, width: 150,),
-        ),
-        SizedBox(height: 20,),
-        user.service == "EP" ? FlatButton.icon(
-            onPressed: () {
-              Navigator.pushNamed(context, "/editprofile",arguments: user);
-            },
-            icon: Icon(Icons.edit),
-            label: Text(tr.translate("editinfo"))
-        ): SizedBox(height: 0,),
-        SizedBox(height: 20,),
-        FlatButton.icon(
-            onPressed: () async {
-              DBService.dbService.getPopular();
-            },
-            icon: Icon(Icons.pan_tool),
-            label: Text("prueba")
-        ),
-        SizedBox(height: 20,),
-        FlatButton.icon(
-            onPressed: () async {
-              Navigator.pushNamed(context, "/favs");
-            },
-            icon: Icon(Icons.favorite),
-            label: Text("favoritos")
-        ),
-        SizedBox(height: 20,),
-        FlatButton.icon(
-            onPressed: () async {
-              await _auth.signOut();
-            },
-            icon: Icon(Icons.exit_to_app),
-            label: Text(tr.translate("signout"))
-        ),
-      ],
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          SizedBox(height: 50.h,),
+          Container(
+            height: 110.h,
+            width: 255.w,
+            child: Row( mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(height: 67, width: 67,
+                    decoration: new BoxDecoration(
+                        shape: BoxShape.circle,
+                        boxShadow: [BoxShadow(
+                          color: Colors.grey.withOpacity(0.2),
+                          spreadRadius: 2,
+                          blurRadius: 3,
+                          offset: Offset(1, 1), // changes position of shadow
+                        ),],
+                        image: new DecorationImage(
+                            fit: BoxFit.cover,
+                            image: new NetworkImage(
+                                user.picture)
+                        )
+                    )
+                ),
+                SizedBox(width: 50.w,),
+                Column(
+                  children: [
+                    SizedBox(height: 20.h,),
+                    Text("${user.username}", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Color.fromRGBO(255, 110, 117, 0.9), letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    Text("${user.ratings.length} Ratings", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.normal, fontSize: ScreenUtil().setSp(16),),)),
+                  ],
+                )
+              ],
+            ),
+            decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.all(Radius.circular(30)),
+              boxShadow: [BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(1, 1), // changes position of shadow
+              ),],
+            ),
+          ),
+          SizedBox(height: 50,),
+          Container(
+            height: 440.h,
+            width: 365.w,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+              boxShadow: [BoxShadow(
+                color: Colors.grey.withOpacity(0.2),
+                spreadRadius: 2,
+                blurRadius: 3,
+                offset: Offset(1, 1), // changes position of shadow
+              ),],
+            ),
+            child: Column(children: [
+              GestureDetector(
+                onTap:(){
+
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      SvgPicture.asset("assets/editusers.svg", width: 37.w, height: 37.h,),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Edit user", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 2,),
+              GestureDetector(
+                onTap:(){
+
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      SvgPicture.asset("assets/ratings.svg", width: 37.w, height: 37.h,),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Rating history", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 2,),
+              GestureDetector(
+                onTap:(){
+
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      Container(width: 37.w, height: 37.h, child: Icon(Icons.favorite_outline_sharp, size: ScreenUtil().setSp(35),)),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Favorites", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 2,),
+              GestureDetector(
+                onTap:(){
+
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      SvgPicture.asset("assets/propietario.svg", width: 37.w, height: 37.h,),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Are you a propietary?", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 2,),
+              GestureDetector(
+                onTap:(){
+
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      SvgPicture.asset("assets/points.svg", width: 37.w, height: 37.h,),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Points Program", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+              Divider(thickness: 2,),
+              GestureDetector(
+                onTap:(){
+                  _auth.signOut();
+                },
+                child: Container(
+                  width: 365.w,
+                  height: 60.h,
+                  child: Row(mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      SizedBox(width: 20.w,),
+                      Container(width: 37.w, height: 37.h, child: Icon(Icons.logout, size: ScreenUtil().setSp(35),)),
+                      SizedBox(width: 30.w,),
+                      Container(width: 250.w, child: Text("Log out", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),))),
+                      Text(">", maxLines: 1, style: GoogleFonts.niramit(textStyle: TextStyle(color: Colors.black, letterSpacing: .3, fontWeight: FontWeight.bold, fontSize: ScreenUtil().setSp(18),),)),
+                    ],
+                  ),
+                ),
+              ),
+            ],),
+          ),
+        ],
+      ),
     );
   }
 
